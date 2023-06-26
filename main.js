415c04410961f53db4627cab9aa38ecd38b6d226
R.Kurkov <ruskura@icloud.com>
2023-06-26 10:00:34 +0300

    3LR

--- main.js
+++ main.js
@@ -1,68 +1,61 @@
-//Задача №1 Функция конвертации скоростей
+const modal = document.getElementById("myModal");
+const btn = document.getElementsByClassName("button1");
+const modal2 = document.querySelector(".modal_body");
 
-function convertSpeed(speed, conv) {
-    if (conv == "toMS") {
-        return speed/3.6;
-    }
-    else if (conv == "toKMH") {
-        return speed*3.6;
-    }
-}
+btn[0].addEventListener("click", () => {
+	modal.style.display = "block";
+});
 
-console.log(`convertSpeed(36, 'toMS') -> ${convertSpeed(36, 'toMS')} м/с`);
-console.log(`convertSpeed(20, 'toKMH') -> ${convertSpeed(20, 'toKMH')} км/ч`);
+modal2.addEventListener('click', (e) => {
+	if (e.target == modal2){
+		modal.style.display = "none";	
+	}
+});
 
-//Задача №2 Абсолютное значение
+const password = document.getElementById("pass");
+const checkPass = document.getElementById("checkPass");
 
-function abs(number) {
-    let result = number;
-    if (number < 0) {
-        result = number * -1;
-    }
-    return result;
-}
+checkPass.addEventListener("pointerdown", () => {
+	pass.type = "text";
+});
 
-console.log(`absValue(-2) -> ${abs(-2)}`);
-console.log(`absValue(100) -> ${abs(100)}`);
-console.log(`absValue(0) -> ${abs(0)}`);
+checkPass.addEventListener("pointerup", () => {
+	pass.type = "password";
+});
 
-//Задача №3 Работа с объектом 
+const error = document.getElementById("error");
+const mail = document.getElementById("mail");
 
-function Student(group,last_name,first_name) {
-    this.group = group;
-    this.last_name = last_name;
-    this.first_name = first_name;
-}
+mail.addEventListener("blur", () => {
+	if (mail.validity.typeMismatch) {
+		error.textContent = "Ошибка! Проверьте адресс электронной почты.";
+		mail.setCustomValidity("Ошибка! Проверьте адресс электронной почты.");
+	} else if (mail.validity.valueMissing) {
+		error.textContent = "Ошибка! Заполните поле.";
+		mail.setCustomValidity("Ошибка! Заполните поле.");
+	} else {
+		error.textContent = "";
+		mail.setCustomValidity("");
+	}
+});
 
-let student = new Student("211-325","Kurkov","Ruslan");
+pass.addEventListener("blur", () => {
+	if (pass.validity.tooShort) {
+		error.textContent = "Ошибка! Минимум 6 символов.";
+		pass.setCustomValidity("Ошибка! Минимум 6 символов.");
+	} else if (pass.validity.valueMissing) {
+		error.textContent = "Ошибка! Заполните поле.";
+		pass.setCustomValidity("Ошибка! Заполните поле.");
+	} else {
+		error.textContent = "";
+		pass.setCustomValidity("");
+	}
+});
 
-console.log(`Список свойств: ${Object.keys(student)}`)
-console.log(`Студент ${student.first_name} ${student.last_name} учится в ${student.group} группе`);
+const form = document.getElementsByClassName('modal_form')
 
-//Задача №4 Случайные числа
-
-function randomNumber (min, max) {
-    let random = min + Math.random() * (max - min + 1);
-    return Math.floor(random);
-}
-
-console.log(`randomNumber(0, 10) -> ${randomNumber(0, 10)}`);
-console.log(`randomNumber(-10, 10) -> ${randomNumber(-10, 10)}`);
-
-//Задача №5 Значения из массива
-
-let arr1 = [1,2,3,4];
-let num1 = 2;
-let arr2 = [1,2,3,4];
-let num2 = 3;
-
-function newArr(arr, num) {
-    let resArr = []
-    for (let i = 0; i < num; i++) {
-        resArr.push(arr[randomNumber(0, arr.length - 1)]);
-    }
-    return resArr;
-}
-
-console.log(`[${arr1}], ${num1}) -> [${newArr(arr1, num1)}]`);
-console.log(`[${arr2}], ${num2}) -> [${newArr(arr2, num2)}]`);
+form[0].addEventListener("submit", (e) =>{
+	let data = new FormData(form);
+	console.log(data.getAll());
+	e.preventDefault()
+}) 
